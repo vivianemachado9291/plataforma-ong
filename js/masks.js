0151331a3ef8a2780383b4312c5dae42388e5e6a
Viviane Machado <vi.ma.silvaa9291@gmail.com>
2025-10-18 09:30:43 -0300

    Update masks.js

--- js/masks.js
+++ js/masks.js
@@ -1,26 +1,28 @@
-// Aplica máscara conforme o usuário digita (evento delegado no documento)
-document.addEventListener("input", function (e) {
-  // CPF -> 000.000.000-00
-  if (e.target.id === "cpf") {
-    e.target.value = e.target.value
+// Máscaras: CPF, Telefone e CEP
+document.addEventListener("input", (e) => {
+  const el = e.target;
+
+  if (el.id === "cpf") {
+    el.value = el.value
       .replace(/\D/g, "")
       .replace(/(\d{3})(\d)/, "$1.$2")
       .replace(/(\d{3})(\d)/, "$1.$2")
-      .replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+      .replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+      .slice(0, 14);
   }
 
-  // Telefone -> (00) 00000-0000
-  if (e.target.id === "telefone") {
-    e.target.value = e.target.value
+  if (el.id === "telefone") {
+    el.value = el.value
       .replace(/\D/g, "")
       .replace(/(\d{2})(\d)/, "($1) $2")
-      .replace(/(\d{5})(\d{4})$/, "$1-$2");
+      .replace(/(\d{5})(\d{1,4})$/, "$1-$2")
+      .slice(0, 15);
   }
 
-  // CEP -> 00000-000
-  if (e.target.id === "cep") {
-    e.target.value = e.target.value
+  if (el.id === "cep") {
+    el.value = el.value
       .replace(/\D/g, "")
-      .replace(/(\d{5})(\d{3})$/, "$1-$2");
+      .replace(/(\d{5})(\d{1,3})$/, "$1-$2")
+      .slice(0, 9);
   }
 });
